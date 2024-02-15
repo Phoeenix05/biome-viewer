f3b41965f3ee8c33af013b2c547d94c650431739
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-15 22:11:07 +0200

    prob: tried to get rendering working

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 extern crate nalgebra_glm as glm;
 
-use eframe::egui::SidePanel;
+use eframe::egui::{RichText, SidePanel};
 use eframe::glow::HasContext;
 use eframe::App;
 
@@ -16,19 +16,21 @@ struct BiomeViewer {
 impl App for BiomeViewer {
     fn update(&mut self, cx: &eframe::egui::Context, frame: &mut eframe::Frame) {
         let size = cx.input(|i| i.viewport().outer_rect).unwrap();
-        let panel_width: f32 = size.width() / 4.0;
+        let panel_width: f32 = size.width() / 4.0; // this is required to determine
+                                                   // where to put the gl vieport
 
         SidePanel::left("left_panel")
             .resizable(false)
             .exact_width(panel_width)
-            .show(cx, |_ui| {});
+            .show(cx, |ui| {
+                ui.label(RichText::new("biome-viewer").size(20.0));
+            });
 
         // canvas
         use eframe::glow::HasContext as _;
         let gl = frame.gl().unwrap();
 
         unsafe {
-            gl.use_program(self.graphics.program);
             gl.disable(eframe::glow::SCISSOR_TEST);
             gl.viewport(
                 panel_width as i32,
@@ -38,17 +40,27 @@ impl App for BiomeViewer {
             );
             gl.clear_color(0.0, 0.0, 0.0, 1.0);
             gl.clear(eframe::glow::COLOR_BUFFER_BIT);
+
+            // setup
+            gl.use_program(Some(self.graphics.program));
+
+            // TODO: drawing
+
+            // cleanup
+            gl.use_program(None);
         }
     }
 
     fn on_exit(&mut self, gl: Option<&eframe::glow::Context>) {
-        unsafe { gl.unwrap().delete_program(self.graphics.program.unwrap()) };
+        unsafe { gl.unwrap().delete_program(self.graphics.program) };
     }
 }
 
 struct Graphics {
     offset: glm::Vec2,
-    program: Option<eframe::glow::NativeProgram>,
+    program: eframe::glow::NativeProgram,
+    vao: eframe::glow::NativeVertexArray,
+    vbo: eframe::glow::NativeBuffer,
 }
 
 fn main() {
@@ -78,27 +90,30 @@ fn main() {
                 (eframe::glow::FRAGMENT_SHADER, FRAGMENT_SHADER),
             ];
 
-            let shaders: Vec<_> = shader_sources
-                .iter()
-                .map(|(shader_type, source)| unsafe {
-                    let shader = gl
-                        .create_shader(*shader_type)
-                        .expect("failed to create shader");
+            let shader_map = |(shader_type, source): &(u32, &str)| unsafe {
+                let shader = gl
+                    .create_shader(*shader_type)
+                    .expect("failed to create shader");
 
-                    gl.shader_source(shader, source);
-                    gl.compile_shader(shader);
-                    assert!(
-                        gl.get_shader_compile_status(shader),
-                        "failed to compile {shader_type}: {}",
-                        gl.get_shader_info_log(shader)
-                    );
+                gl.shader_source(shader, source);
+                gl.compile_shader(shader);
+                assert!(
+                    gl.get_shader_compile_status(shader),
+                    "failed to compile {shader_type}: {}",
+                    gl.get_shader_info_log(shader)
+                );
 
-                    gl.attach_shader(program, shader);
-                    shader
-                })
-                .collect();
+                gl.attach_shader(program, shader);
+                shader
+            };
+
+            let shaders: Vec<_> = shader_sources.iter().map(shader_map).collect();
 
             unsafe {
+                // these two lines as `layout(location = ...)` doesn't seem to work
+                gl.bind_attrib_location(program, 0, "position");
+                gl.bind_attrib_location(program, 1, "color");
+
                 gl.link_program(program);
                 assert!(
                     gl.get_program_link_status(program),
@@ -112,13 +127,40 @@ fn main() {
                 }
             }
 
-            // initialize the actual application state
-            let graphics = Graphics {
-                offset: glm::Vec2::zeros(),
-                program: Some(program),
+            let (vao, vbo) = unsafe {
+                let vao = gl.create_vertex_array().unwrap();
+                gl.bind_vertex_array(Some(vao));
+
+                let vbo = gl.create_buffer().unwrap();
+                gl.bind_buffer(eframe::glow::ARRAY_BUFFER, Some(vbo));
+                gl.buffer_data_u8_slice(
+                    eframe::glow::ARRAY_BUFFER,
+                    bytemuck::cast_slice(&[0.0, 1.0, -1.0, -1.0, 1.0, -1.0]),
+                    eframe::glow::STATIC_DRAW,
+                );
+
+                // Position attribute (location = 0)
+                gl.enable_vertex_attrib_array(0);
+                gl.vertex_attrib_pointer_f32(0, 2, eframe::glow::FLOAT, false, 0, 0);
+                gl.vertex_attrib_divisor(0, 1);
+
+                // Color attribute (location = 1)
+                gl.enable_vertex_attrib_array(1);
+                gl.vertex_attrib_pointer_f32(1, 4, eframe::glow::FLOAT, false, 0, 0);
+                gl.vertex_attrib_divisor(1, 1);
+
+                (vao, vbo)
             };
 
-            Box::new(BiomeViewer { graphics })
+            // initialize the actual application state;
+            Box::new(BiomeViewer {
+                graphics: Graphics {
+                    offset: glm::Vec2::zeros(),
+                    program,
+                    vao,
+                    vbo,
+                },
+            })
         }),
     )
     .map_err(|err| eprintln!("{}", err));
